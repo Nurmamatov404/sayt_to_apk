a7a7e9ef4479b9c129d9491ac0639d8fbfd79780
Nurmamatov404 <nurmamatovbahodir2008@gmail.com>
2026-05-02 00:56:34 +0500

    first upload

---
+++ bot/main.py
@@ -0,0 +1,39 @@
+import requests
+from telethon import TelegramClient, events
+
+api_id = 20464354
+api_hash = "c6fa656e333fd6c9d5b9867daf028ea1"
+bot_token = "SENING_BOT_TOKENING"
+
+GITHUB_TOKEN = "GITHUB_TOKEN"
+REPO = "username/repo"
+
+client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+
+@client.on(events.NewMessage)
+async def handler(event):
+    url = event.raw_text.strip()
+
+    if not url.startswith("http"):
+        await event.reply("Link yubor ❌")
+        return
+
+    await event.reply("⏳ APK tayyorlanmoqda...")
+
+    # GitHub Action trigger
+    requests.post(
+        f"https://api.github.com/repos/{REPO}/dispatches",
+        headers={
+            "Authorization": f"token {GITHUB_TOKEN}",
+            "Accept": "application/vnd.github.v3+json"
+        },
+        json={
+            "event_type": "build_apk",
+            "client_payload": {
+                "url": url,
+                "chat_id": event.chat_id
+            }
+        }
+    )
+
+client.run_until_disconnected()
